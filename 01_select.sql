76807d2c785097c0ce80b0000a00d0f2d807c2d6
thiago-laza <thiago.profmat@gmail.com>
2025-09-10 20:26:11 -0300

    envio dos arquivos

---
+++ 01_select.sql
@@ -0,0 +1,12 @@
+--SELECT * FROM clientes
+
+--SELECT IdCliente, QtdePontos FROM clientes
+
+--SELECT 
+    --IdCliente, 
+    --QtdePontos, 
+   -- DtCriacao 
+
+--FROM clientes
+
+SELECT * FROM clientes LIMIT 10
