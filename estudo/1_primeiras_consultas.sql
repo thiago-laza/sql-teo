15b000c851fe9bdab01e32cad3a669b80dbd4a20
thiago-laza <thiago.profmat@gmail.com>
2025-09-16 17:48:21 -0300

    concluido SELECT, FROM, WHERE, CASE WHEN

--- estudo/1_primeiras_consultas.sql
+++ estudo/1_primeiras_consultas.sql
@@ -62,3 +62,236 @@ SELECT
     END AS NivelCliente
 FROM 
     clientes;
+
+    -- EXERCICIOS
+
+-- 1. Exercícios com SELECT e FROM
+
+--1. Selecione todas as colunas da tabela clientes.
+
+SELECT * FROM clientes;
+
+-- 2. Exiba apenas o IdCliente e a DtCriacao de todos os registros.
+
+SELECT IdCliente, DtCriacao  FROM clientes;
+
+-- 3. Selecione a quantidade de pontos (QtdePontos) e a data de atualização (DtAtualizacao) dos clientes.
+
+SELECT QtdePontos, DtAtualizacao FROM clientes;
+
+-- 4. Exiba o IdCliente e as flags FlEmail e FlTwitch.
+
+SELECT IdCliente, FlEmail, FlTwitch FROM clientes;
+
+-- 5. Selecione apenas as colunas FlYouTube e FlInstagram.
+
+SELECT FlYouTube, FlInstagram FROM clientes;
+
+-- 6. Liste todos os valores únicos na coluna FlEmail.
+
+SELECT DISTINCT FlEmail FROM clientes;
+
+-- 7. Selecione todos os dados dos clientes, mas mostre a coluna QtdePontos como a primeira coluna.
+
+SELECT
+  QtdePontos,
+  IdCliente,
+  FlEmail,
+  FlTwitch,
+  FlYouTube,
+  FlBlueSky,
+  FlInstagram,
+  DtCriacao,
+  DtAtualizacao
+FROM 
+  clientes;
+
+-- 8. Exiba apenas a coluna QtdePontos de todos os clientes.
+
+SELECT QtdePontos FROM clientes;
+
+-- 9. Liste todos os valores únicos na coluna FlTwitch.
+
+SELECT DISTINCT FlTwitch FROM clientes;
+
+-- 10. Selecione as colunas IdCliente, DtCriacao, e QtdePontos.
+
+SELECT IdCliente, DtCriacao, QtdePontos FROM clientes;
+
+
+-- 2. Exercícios com WHERE
+
+-- 1. Selecione todos os clientes com mais de 500 pontos.
+
+SELECT * FROM clientes WHERE QtdePontos > 500;
+
+-- 2. Exiba os clientes que têm exatamente 100 pontos.
+
+SELECT * FROM clientes WHERE QtdePontos = 100;
+
+-- 3. Liste os clientes que têm 0 pontos e FlTwitch ativada (= 1).
+
+SELECT * FROM clientes WHERE QtdePontos = 0 AND FlTwitch = 1;
+
+-- 4. Selecione todos os clientes que foram criados após a data '2025-01-01 00:00:00 +0000 UTC'.
+
+SELECT * FROM clientes WHERE DtCriacao > '2025-01-01 00:00:00 +0000 UTC'
+
+-- 5. Exiba os clientes com pontuação entre 50 e 100, incluindo 50 e 100.
+
+SELECT * FROM clientes WHERE QtdePontos BETWEEN 50 AND 100;
+
+-- 6. Selecione os clientes que têm a flag FlInstagram ativada (= 1).
+
+SELECT * FROM clientes WHERE FlInstagram = 1;
+
+-- 7. Selecione os clientes que têm a flag FlEmail ativada (= 1) OU a flag FlTwitch ativada (= 1).
+
+SELECT * FROM clientes WHERE FlEmail = 1 OR FlTwitch =1;
+
+-- 8. Liste os clientes com mais de 50 pontos E FlYouTube ativada (= 1).
+
+SELECT * FROM clientes WHERE QtdePontos > 50 AND FlYouTube = 1;
+
+-- 9. Exiba os clientes que têm a QtdePontos igual a 0.
+
+SELECT * FROM clientes WHERE QtdePontos = 0;
+
+-- 10. Selecione os clientes que não têm a flag FlBlueSky ativada.
+
+SELECT * FROM clientes WHERE FlBlueSky = 0;
+
+-- 3. Exercícios com CASE WHEN
+
+-- 1. Crie uma coluna chamada StatusAtividade na qual, se DtAtualizacao for igual a 
+--DtCriacao, o valor seja "Não Atualizado", caso contrário seja "Atualizado".
+
+
+SELECT 
+  DtCriacao,
+  DtAtualizacao,
+  CASE
+    WHEN DtCriacao = DtAtualizacao THEN 'nao atualizado'
+    ELSE 'atualizado'
+  END AS StatusAtividade
+FROM
+  clientes;
+
+
+-- 2. Adicione uma coluna chamada PlataformaAtiva na qual, se FlTwitch for 1, 
+-- o valor seja "Twitch", caso contrário seja "Outra".
+
+SELECT
+  FlTwitch,
+  CASE
+    WHEN FlTwitch = 1 THEN 'twitch'
+    ELSE 'outra'
+  END AS PlataformaAtiva
+from 
+  clientes;
+
+-- 3. Crie uma coluna CategoriaPontos na qual, se QtdePontos for maior ou igual a 100, 
+--o valor seja "Premium", caso contrário "Básico".
+
+SELECT
+  QtdePontos,
+  CASE
+    WHEN QtdePontos >= 100 THEN 'Premium'
+    ELSE 'Basico'
+  END AS CategoriaPontos
+FROM
+  clientes;
+
+-- 4. Adicione uma coluna chamada StatusTwitch na qual, se FlTwitch for 1, 
+--o valor seja "Ativo", caso contrário, "Inativo".
+
+SELECT
+  FlTwitch,
+  CASE
+    WHEN FlTwitch = 1 THEN 'ativo'
+    ELSE 'inativo'
+  END AS StatusTwitch
+FROM
+  clientes;
+
+-- 5. Crie uma coluna UsaMidiasSociais na qual, se FlInstagram ou 
+--FlYouTube ou FlTwitch for 1, o valor seja "Sim", caso contrário "Não".
+
+SELECT
+  FlInstagram,
+  FlYouTube,
+  FlTwitch,
+  CASE
+    WHEN FlInstagram = 1 THEN 'sim'
+    WHEN FlYouTube = 1 THEN 'sim'
+    WHEN FlTwitch = 1  THEN 'sim'
+    ELSE 'nao'
+  END AS UsaMididasSociais
+FROM
+  clientes;
+
+-- 6. Adicione uma coluna chamada TipoCliente. Se QtdePontos for maior que 1000, 
+--o valor será "Nível A", se for maior que 500, será "Nível B", e senão, "Nível C".
+
+SELECT
+  QtdePontos,
+  CASE
+    WHEN QtdePontos > 1000 THEN 'nivel A'
+    WHEN QtdePontos > 500 THEN 'nivel B'
+    ELSE 'nivel C'
+  END AS TipoCliente
+FROM
+  clientes;
+
+-- 7. Crie uma coluna UsaRede na qual, se FlEmail for 1, o valor seja "Email", 
+--se FlTwitch for 1, seja "Twitch", e se FlYouTube for 1, seja "YouTube". Se nenhuma for 1, o valor será "Nenhuma".
+
+SELECT
+  FlEmail,
+  FlTwitch,
+  FlYouTube,
+  CASE
+    WHEN FlEmail = 1 THEN 'email'
+    WHEN FlTwitch = 1 THEN 'twitch'
+    WHEN FlYouTube = 1 THEN 'youtube'
+    ELSE 'nenhuma'
+  END AS UsaRede
+FROM
+  clientes;
+
+-- 8. Adicione uma coluna ContaAntiga na qual, se a DtCriacao for anterior a 
+--'2025-01-01 00:00:00 +0000 UTC', o valor será "Sim", caso contrário "Não".
+
+SELECT
+  DtCriacao,
+  CASE
+    WHEN DtCriacao < '2025-01-01 00:00:00 +0000 UTC' THEN 'sim'
+    ELSE 'nao'
+  END AS ContaAntiga
+FROM
+  clientes;
+
+-- 9. Crie uma coluna Prioridade na qual, se FlEmail for 1 
+-- e QtdePontos for maior que 50, o valor seja "Alta Prioridade", caso contrário "Normal".
+
+SELECT
+  FlEmail,
+  QtdePontos,
+  CASE
+    WHEN FlEmail = 1 AND QtdePontos > 50 THEN 'alta prioridade'
+    ELSE 'normal'
+  END AS Prioridade
+FROM
+  clientes;
+
+-- 10. Crie uma coluna TipoUsuario na qual, se QtdePontos for 0, 
+-- o valor seja "Iniciante", e se for maior que 0, "Participante".
+
+SELECT
+  QtdePontos,
+  CASE
+    WHEN QtdePontos = 0 THEN 'iniciante'
+    ELSE 'participante'
+  END AS TipoUsuario
+FROM
+  clientes;
